5ffdba283cacc36231a86057c83b2292245a4448
prakash <prakashvadakadu8@gmail.com>
2025-06-01 09:46:39 +0530

    schema updated

--- models/product.model.js
+++ models/product.model.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema(
     listingType: {
       type: String,
       required: true,
-      enum: ["skateboard", "bike", "shoe", "other","clothing"],
+      enum: ["skateboard", "bike", "shoe", "other","clothing","headphone"],
     },
   },
   { timestamps: true }
