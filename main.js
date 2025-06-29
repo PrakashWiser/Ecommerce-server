039d4fd04870acea2f79869098f258a8167f75e6
prakash <prakashvadakadu8@gmail.com>
2025-06-29 22:19:13 +0530

    cors origin global to private

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import connectDB from "./lib/db.js";
 import productRoutes from "./routes/product.route.js";
@@ -8,6 +7,7 @@ import { protect } from "./middlewares/auth.middelware.js";
 import cloudinary from "cloudinary";
 import authRoutes from "./routes/auth.route.js";
 import paymentRoutes from "./routes/payment.route.js";
+import compression from "compression";
 import cors from "cors";
 
 cloudinary.config({
@@ -16,17 +16,34 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
 });
 
+dotenv.config();
 connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://ecommerce-page-opal.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "*",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
 
-app.use(express.json({ limit: "50mb" }));
+app.use(compression());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.get("/", (req, res) => {
